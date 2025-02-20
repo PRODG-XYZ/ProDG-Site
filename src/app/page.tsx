2ce022e0451ca263027d8a9ab55693201863d2d7
Mitch <Mitch.Ngugi99@gmail.com>
2025-02-20 18:44:38 +0000

    testimonial removal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -194,14 +194,14 @@ export default async function Home() {
 
       <CaseStudies caseStudies={caseStudies} />
 
-      <Testimonial
+      {/* <Testimonial
         className="mt-24 sm:mt-32 lg:mt-40"
         client={{ name: 'Dealfuze', logo: logoPhobiaDark }}
       >
         The team at ProDG Studio went above and beyond with our onboarding, even
         finding a way to access the user's microphone without triggering one of
         those annoying permission dialogs.
-      </Testimonial>
+      </Testimonial> */}
 
       <Services />
 
